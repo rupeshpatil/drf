f9598960c4a0c46633c7b5e70a97398f679cf404
Rupesh Patil <rupeshrp12@gmail.com>
2020-04-23 10:15:55 +0530

    First commit

---
+++ drf/accounts/views.py
@@ -0,0 +1,25 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from accounts.serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
+
+class UserCreate(APIView):
+    """ 
+    Creates the user. 
+    """
+    permission_classes = (AllowAny,)
+
+    def post(self, request, format='json'):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                token = Token.objects.create(user=user)
+                json = serializer.data
+                json['token'] = token.key
+                return Response(json, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
